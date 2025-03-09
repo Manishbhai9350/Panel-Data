e015835cd76fa00d76625d3b17d9690ed5c53469
MANISH DHAKA <129970261+Manishbhai9350@users.noreply.github.com>
2025-03-09 11:12:38 +0530

    Update SubService.jsx

--- src/app/_components/SubService.jsx
+++ src/app/_components/SubService.jsx
@@ -7,7 +7,7 @@ const SubService = ({price,amount,service,idx}) => {
       style={{ paddingRight: 10,marginBlock:3 }}
       className="sub-service  flex justify-between items-center w-full h-[40px] border-y border-black"
     >
-        <h2 className="leading-3 font-semibold text-xl">{idx} &nbsp; {price}₹</h2>
+        <h2 className="leading-3 font-semibold text-xl"><span className="text-sm opacity-70">{idx + 1}</span> {price}₹</h2>
         <p className="leading-4 text-xl">{amount} {service} </p>
     </div>
   );
