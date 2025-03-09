d11a2687e52d8bd050a8ce4f7363ab5ed5762a29
MANISH DHAKA <129970261+Manishbhai9350@users.noreply.github.com>
2025-03-09 10:45:25 +0530

    Update SubService.jsx

--- src/app/_components/SubService.jsx
+++ src/app/_components/SubService.jsx
@@ -1,13 +1,13 @@
 'use client'
 import React from "react";
 
-const SubService = ({price,amount,service}) => {
+const SubService = ({price,amount,service,idx}) => {
   return (
     <div
       style={{ paddingRight: 10,marginBlock:3 }}
       className="sub-service  flex justify-between items-center w-full h-[40px] border-y border-black"
     >
-        <h2 className="leading-3 font-semibold text-xl">{price}₹</h2>
+        <h2 className="leading-3 font-semibold text-xl">{idx} &nbsp; {price}₹</h2>
         <p className="leading-4 text-xl">{amount} {service} </p>
     </div>
   );
